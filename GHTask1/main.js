bcd02b30401540c439ff867238b8195fc3e3e126
Kate <yatsenko.km@gmail.com>
2018-10-17 15:09:16 +0300

    1 task done

--- GHTask1/main.js
+++ GHTask1/main.js
@@ -1,16 +1,29 @@
 var n = 1000;
-var funcCode = 'switch (x){';
-for (var x = 1; x <= n; x++) {
-    funcCode += `case ${x}:break;`;
+
+var ifFuncCode = '';
+var switchFuncCode = 'switch (y){';
+for (var x = 0; x < n; x++) {
+    ifFuncCode += `if(y == ${x}){}`;
+    switchFuncCode += `case ${x} :break;`;
 }
-funcCode += '}';
+switchFuncCode += '}';
 
 function switchMultipleCall() {
-    for (var i = 1; i <= n; i++) {
-        var switchOneCall = new Function('x', funcCode)(i);
+    for (var i = 0; i < n; i++) {
+        var switchOneCall = new Function('y', switchFuncCode)(i);
+    }
+}
+
+function ifMultipleCall() {
+    for (var i = 0; i < n; i++) {
+        var ifOneCall = new Function('y', ifFuncCode)(i);
     }
 }
 
 console.time('switchTime');
 switchMultipleCall();
 console.timeEnd('switchTime');
+
+console.time('ifTime');
+ifMultipleCall();
+console.timeEnd('ifTime');
