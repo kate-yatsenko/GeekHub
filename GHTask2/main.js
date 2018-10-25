28d373038667141afe909e127e99330a177501e7
Kate <yatsenko.km@gmail.com>
2018-10-25 17:05:06 +0300

    second task initial version

---
+++ GHTask2/main.js
@@ -0,0 +1,36 @@
+var string = prompt('Enter your string:', '');
+
+function stringCheck(str) {
+    var el = [];
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "{" || str[i] == "[" || str[i] == "(" || str[i] == "'" || str[i] == "`" || str[i] == "\"") {
+            el.push(str[i]);
+        } else if (str[i] == "}") {
+            if (el[el.length - 1] == "{") {
+                el.pop();
+            }
+        } else if (str[i] == "]") {
+            if (el[el.length - 1] == "[") {
+                el.pop();
+            }
+        } else if (str[i] == ")") {
+            if (el[el.length - 1] == "(") {
+                el.pop();
+            }
+        }
+        if (el[el.length - 1] == "'" && el[el.length - 2] == "'" || el[el.length - 1] == "`" && el[el.length - 2] == "`" || el[el.length - 1] == "\"" && el[el.length - 2] == "\"") {
+            el.splice([el.length - 2]);
+        }
+        if (el[el.length - 3] == "'" && el[el.length - 1] == "'" || el[el.length - 3] == "`" && el[el.length - 1] == "`" || el[el.length - 3] == "\"" && el[el.length - 1] == "\"") {
+            el.splice([el.length - 3]);
+        }
+    }
+    return el;
+}
+
+var result = stringCheck(string);
+if (result.length == 0) {
+    console.log('Code is correct!');
+} else {
+    alert('Don\'t close: ' + result);
+}
